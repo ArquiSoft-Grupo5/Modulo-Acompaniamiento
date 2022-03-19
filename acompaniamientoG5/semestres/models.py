642cd323c14816413ade03bf89854f46721e7fa8
Maria Paula <m.gonzaleze@uniandes.edu.co>
2022-03-19 00:07:44 -0500

    Merge branch 'main' of https://github.com/ArquiSoft-Grupo5/Modulo-Acompaniamiento into main

--- acompaniamientoG5/semestres/models.py
+++ acompaniamientoG5/semestres/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+from estudiantes.models import Estudiante
+
+class Semestre(models.Model):
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE, default=None)
+    valor = models.FloatField(null=True, blank=True, default=None)
+    periodo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '%s %s' % (self.valor, self.periodo)
