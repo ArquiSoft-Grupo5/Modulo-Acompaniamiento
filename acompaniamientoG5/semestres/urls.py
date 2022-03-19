6514684d1b5bf5dd7ecf56c55e0c460f95bb407d
WilliamMendez <69637352+WilliamMendez@users.noreply.github.com>
2022-03-19 02:53:27 -0500

    aesdfghjklñ{

--- acompaniamientoG5/semestres/urls.py
+++ acompaniamientoG5/semestres/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from . import views
 urlpatterns = [
     path('', views.semestres_view, name='semestres_view'),
     path('<int:pk>', views.semestre_view, name='semestre_view'),
+    path('semestres/create/', csrf_exempt(views.semestres_view), name='semestreCreate'),
 ]
