32a7bafb421adfc6ee03086c9e2f0576a102c15b
WilliamMendez <69637352+WilliamMendez@users.noreply.github.com>
2022-03-18 21:18:55 -0500

    Update settings.py

--- acompaniamientoG5/acompaniamientoG5/settings.py
+++ acompaniamientoG5/acompaniamientoG5/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'estudiantes',
+    'semestres',
+    'usuarios',
 ]
 
 MIDDLEWARE = [
