a14916f5eadb9ee305440a783c9f3a0a66c53a23
WilliamMendez <69637352+WilliamMendez@users.noreply.github.com>
2022-04-06 22:05:56 -0500

    rgfsegr

---
+++ acompaniamientoG5/preAnalisis/logic/preAnalisis_logic.py
@@ -0,0 +1,20 @@
+from ..models import PreAnalisis
+
+def get_preAnalisiss():
+    preAnalisis = PreAnalisis.objects.all()
+    return preAnalisis
+
+def get_preAnalisis(var_pk):
+    preAnalisis = PreAnalisis.objects.get(pk=var_pk)
+    return preAnalisis
+
+def update_preAnalisis(var_pk, new_var):
+    preAnalisis = get_preAnalisis(var_pk)
+    preAnalisis.name = new_var["name"]
+    preAnalisis.save()
+    return preAnalisis
+
+def create_preAnalisis(var):
+    preAnalisis = PreAnalisis(name=var["name"])
+    preAnalisis.save()
+    return preAnalisis
