1718129afcb4d2e8f55234be73064b769203bda7
William Mendez <69637352+WilliamMendez@users.noreply.github.com>
2022-04-25 09:04:35 -0500

    Delete acompaniamientoG5/usuarios directory

--- acompaniamientoG5/usuarios/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Usuario (models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return '{}'.format(self.name)
